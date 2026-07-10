d131820b06a27ff611ad82eff9bf6b2f7056772c
whyme-blip <whymetrymenow@gmail.com>
2026-07-10 14:58:38 +0530

    Refactor DATASETS to use ES6 module syntax

--- tests/fixtures/index.js
+++ tests/fixtures/index.js
@@ -7,7 +7,7 @@ const createMockRecords = () => [
     { station: 'ST-01', structure: 'foliation', strike: '148', dip: '28', generation: 'F1', domain: 'Global', lithology: 'Schist' }
 ];
 
-const DATASETS = {
+export const DATASETS = {
     PointCluster: (rng) => createMockRecords(),
     WeakGirdle: (rng) => createMockRecords(),
     StrongGirdle: (rng) => createMockRecords(),
@@ -16,12 +16,13 @@ const DATASETS = {
     TwoDomain: (rng) => createMockRecords()
 };
 
-function getFixtures() { return DATASETS; }
-function loadFixtures() { return DATASETS; }
+export const datasets = DATASETS;
+export function getFixtures() { return DATASETS; }
+export function loadFixtures() { return DATASETS; }
 
-module.exports = {
+export default {
     DATASETS,
-    datasets: DATASETS,
+    datasets,
     getFixtures,
     loadFixtures
 };
